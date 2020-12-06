97140e013790fd71e5efe34e908fd7b6a2462dc8
FromSouthToNorth <598050554@qq.com>
2020-12-06 21:57:21 +0800

    add dataSql goosip_table

--- sql/20201126.sql
+++ sql/20201126.sql
@@ -689,3 +689,19 @@ create table sys_comment (
     create_time 	datetime                                    comment '创建时间',
     primary key (comment_id)
 ) engine=innodb auto_increment=100 comment = '博客评论表';
+
+-- ----------------------------
+-- 25、随笔表
+-- ----------------------------
+drop table if exists sys_gossip;
+create table sys_gossip (
+    gossip_id           bigint(20)      not null    auto_increment  comment '随笔id',
+    gossip_content      varchar(600)    not null                    comment '随笔内容',
+    like_number         int(11)         default 0                   comment '随笔点赞数',
+    del_flag            char(1)         default '0'                 comment '删除标志（0代表存在 2代表删除）',
+    create_by           varchar(64)     default ''                  comment '创建者',
+    create_time 	    datetime                                    comment '创建时间',
+    update_by           varchar(64)     default ''                  comment '更新者',
+    update_time         datetime                                    comment '更新时间',
+    primary key (gossip_id)
+) engine=innodb auto_increment=100 comment = '随笔表';
